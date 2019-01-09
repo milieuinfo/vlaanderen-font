89ca877cb6ee4b4937e3a341e50e37385e50c27b
Tom Coemans <tom.coemans@gmail.com>
2019-01-09 14:35:39 +0100

    GVR-185 rekening houden met hostname

--- test/vlaanderen-font.test.js
+++ test/vlaanderen-font.test.js
@@ -39,7 +39,21 @@ suite('browser support', function() {
             assert.exists(link);
             assert.equal(link.getAttribute('rel'), 'stylesheet');
             assert.equal(link.getAttribute('type'), 'text/css');
-            assert.equal(link.getAttribute('href'), 'https://cdn.milieuinfo.be/vlaanderen-font/LATEST/style.css');
+            assert.equal(link.getAttribute('href'), 'https://cdn.milieuinfo.be/vlaanderen-font/LATEST/milieuinfo.style.css');
+            done();
+        });
+    });
+
+    test('wanneer de host ruimteinfo is, zal er een ander style script gebruikt worden', (done) => {
+        const dom = setup();
+        dom.reconfigure({url: 'https://app.ruimteinfo.be'});
+        wait(dom, () => {
+            const window = dom.window;
+            const link = window.document.querySelector('link');
+            assert.exists(link);
+            assert.equal(link.getAttribute('rel'), 'stylesheet');
+            assert.equal(link.getAttribute('type'), 'text/css');
+            assert.equal(link.getAttribute('href'), 'https://cdn.ruimteinfo.be/vlaanderen-font/LATEST/ruimteinfo.style.css');
             done();
         });
     });
--- vlaanderen-font.js
+++ vlaanderen-font.js
@@ -8,7 +8,11 @@
         var link = document.createElement('link');
         link.setAttribute('rel', 'stylesheet');
         link.setAttribute('type', 'text/css');
-        link.setAttribute('href', 'https://cdn.milieuinfo.be/vlaanderen-font/LATEST/style.css');
+        link.setAttribute('href', getStyleLink());
         return link;
+    }
+
+    function getStyleLink() {
+        return window.location.hostname.indexOf('.ruimteinfo.be') > 0 ? 'https://cdn.ruimteinfo.be/vlaanderen-font/LATEST/ruimteinfo.style.css' : 'https://cdn.milieuinfo.be/vlaanderen-font/LATEST/milieuinfo.style.css';
     }
 })();
